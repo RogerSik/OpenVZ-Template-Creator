c1577ab15574e424b00988185764f2e8208d40f2
Roger Sikorski <roger@sikorski.cc>
2009-12-19 21:03:20 +0100

    debootstrap installation now for another distribution than Ubuntu/Debian.

--- 1_create_system.sh
+++ 1_create_system.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 #
 # Template OS Creator
+# http://github.com/RogerSik/OpenVZ-Template-Creator
 #
 echo "Where to create the system? (default /mnt/dice)"
 read input_path
+
+echo "What is your host distro?"
+echo "Supported: debian | ubuntu"
+read input_host_distri
 
 echo "Create and clear the path"
 mkdir input_path
 rm -rf $input_path/* 
 
-# clear umount
+echo "clear umount"
 umount $input_path/dev
 umount $input_path/proc
 umount $input_path//sys
 
-echo "Download and installatioon the latest debootstrap"
-wget http://files.yoschi.cc/debs/debootstrap.deb
-dpkg -i debootstrap.deb && rm debootstrap.deb
+case "$input_host_distri" in
+     ubuntu|debian)
+		echo "Download and installatioon the latest debootstrap."
+		wget http://files.yoschi.cc/debs/debootstrap.deb
+		dpkg -i debootstrap.deb && rm debootstrap.deb
+		;;
+     *)
+		echo "Distri" $input_host_distri "not supported yet. Sorry."
+		exit 0
+		;; esac
 
 clear
 echo "What distri want you install?"
@@ -31,10 +43,10 @@ debootstrap --arch $input_arch $input_distri $input_path
 
 wget http://files.yoschi.cc/vpsmem
 chmod +x vpsmem
-mv vpsmem /mnt/dice/usr/local/bin
+mv vpsmem $input_path/usr/local/bin
 
-#mount -o bind /dev /mnt/dice/dev
+#mount -o bind /dev $input_path/dev
 mount -t proc none $input_path/proc # because openssh-server will not configured to the end
-#mount -t sysfs none /mnt/dice/sys
+#mount -t sysfs none $input_path/sys
 
 chroot $input_path
