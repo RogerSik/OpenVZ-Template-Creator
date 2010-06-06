d675c558df8dfd7d6e2646fa2445fcd67862bfe9
root <root@rescue.(none)>
2010-06-06 15:16:46 +0200

    Various fixes on all scripts that were adopted to gentoo

--- 3_cleanup_system.sh
+++ 3_cleanup_system.sh
@@ -19,16 +19,23 @@ echo "Which distribution are you installing again?"
 echo "supported: debian ubuntu gentoo"
 read input_distri
 
-case "$inut_distri" in
+case "$input_distri" in
 	debian|ubuntu)
 		# Each individual VE should have its own pair of SSH host keys. 
 		# The code below will wipe out the existing SSH keys and instruct the newly-created VE to create new SSH keys on first boot.
-		cat << EOF > $input_path/etc/rc2.d/S15ssh_gen_host_keys
-		#!/bin/sh
-		ssh-keygen -f /etc/ssh/ssh_host_rsa_key -t rsa -N ''
-		ssh-keygen -f /etc/ssh/ssh_host_dsa_key -t dsa -N ''
-		rm /etc/rc2.d/S15ssh_gen_host_keys
-		EOF
+		#cat << C_EOF > ${input_path}/etc/rc2.d/S15ssh_gen_host_keys
+		##!/bin/sh
+		#ssh-keygen -f /etc/ssh/ssh_host_rsa_key -t rsa -N ''
+		#ssh-keygen -f /etc/ssh/ssh_host_dsa_key -t dsa -N ''
+		#rm /etc/rc2.d/S15ssh_gen_host_keys
+		#C_EOF
+		# looks like bash doesnt like cat << EOF > file input inside a case statement. putting the file line-by-line
+		#cat << C_EOF > ${input_path}/etc/rc2.d/S15ssh_gen_host_keys
+		echo "#!/bin/sh" > ${input_path}/etc/rc2.d/S15ssh_gen_host_keys
+		echo "ssh-keygen -f /etc/ssh/ssh_host_rsa_key -t rsa -N ''" >> ${input_path}/etc/rc2.d/S15ssh_gen_host_keys
+		echo "ssh-keygen -f /etc/ssh/ssh_host_dsa_key -t dsa -N ''" >> ${input_path}/etc/rc2.d/S15ssh_gen_host_keys
+		echo "rm /etc/rc2.d/S15ssh_gen_host_keys" >> ${input_path}/etc/rc2.d/S15ssh_gen_host_keys
+
 		chmod +x $input_path/etc/rc2.d/S15ssh_gen_host_keys
 		cd $input_path/root/
 		> .bash_history; > .viminfo
@@ -39,7 +46,7 @@ case "$inut_distri" in
 		;; #END debian|ubuntu
 	gentoo)
 		cd $input_path/var/log/
-		> lastlog; > faillog; >wtmp
+		> lastlog; > faillog; > wtmp
 		> auth.log; > cron.log; > daemon.log; > debug; > dmesg; > mail.log; > messages; > syslog; > user.log
 		> emerge-fetch.log; > emerge.log; > portage/elog/summary.log
 		cd $input_path/usr/portage/distfiles/
@@ -48,9 +55,15 @@ case "$inut_distri" in
 		> .bash_history
 		rm -ri .ssh
 		;; #END gentoo
-
+	*)
+		echo "Sorry" $input_distri "is not supported"
+		exit 0;
+		;; esac
 
 echo "Whats the name for that template? (without tar.gz!)"
 echo "example ubuntu-8.04.3-i386"
 read input_template_name
-cd $input_path &&  tar --numeric-owner -zcf ~/$input_template_name.tar.gz . && echo $input_template_name".tar.gz saved under ~/"
+cd $input_path 
+tar --numeric-owner -zcf ~/${input_template_name}.tar.gz .
+echo $input_template_name".tar.gz saved under ~/"
+
